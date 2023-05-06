44522ee27db401f1374429d6bbafff9b0555ba3d
gabeFrancisco <gabegit2@gmail.com>
2023-05-06 18:40:37 -0300

    changed mysql module, organizes folders and modules

--- src/server.js
+++ src/server.js
@@ -1,40 +1,6 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const json = express.json
-const app = express()
-const mysql = require('mysql')
+const app = require('./app');
+const dotenv = require('dotenv').config()
 
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'nodeApi',
-  socketPath: '/var/run/mysqld/mysqld.sock'
-})
-
-conn.connect(err => {
-  if(err){
-    console.log(err)
-    return
-  }
-  console.log("Connection established!");
-})
-
-conn.end(err => {
-  if(err){
-    console.log(err)
-    return
-  }
-  console.log("Connection finished!")
-})
-
-const PORT = process.env.PORT || '3500'
-
-app.get('/', (req, res) => {
-  res.send({
-    name: "Jesus",
-    role: "Savior!"
-  })
-})
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => console.log(`Server is runnig at http://localhost:${PORT}/`))
